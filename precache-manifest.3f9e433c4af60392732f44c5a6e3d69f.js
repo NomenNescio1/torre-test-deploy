df7b2e3fdad3a0e703ccea44dcd030d93233b189
NomenNescio <u1201416@unimilitar.edu.co>
2020-01-20 21:37:16 -0500

    Updates

---
+++ precache-manifest.3f9e433c4af60392732f44c5a6e3d69f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b2420c80d258cfd29a9fc745258bef",
+    "url": "/torre-test-deploy/index.html"
+  },
+  {
+    "revision": "0bec3c8f238f600ad746",
+    "url": "/torre-test-deploy/static/css/main.ff0c8c65.chunk.css"
+  },
+  {
+    "revision": "3fa6c79422403206c23f",
+    "url": "/torre-test-deploy/static/js/2.b7177541.chunk.js"
+  },
+  {
+    "revision": "0bec3c8f238f600ad746",
+    "url": "/torre-test-deploy/static/js/main.baca6406.chunk.js"
+  },
+  {
+    "revision": "a57870d8c25d32b22160",
+    "url": "/torre-test-deploy/static/js/runtime-main.840c8443.js"
+  }
+]);
